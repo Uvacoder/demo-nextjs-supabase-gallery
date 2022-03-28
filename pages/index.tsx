bdce47917ac33b4797f413a147fba1f9a2b78e4b
sw-yx <shawnthe1@gmail.com>
2022-03-28 23:35:52 +0800

    types

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,8 +6,8 @@ import { useState } from 'react';
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseAdmin = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
+  process.env.NEXT_PUBLIC_SUPABASE_URL || 'na',
+  process.env.SUPABASE_SERVICE_ROLE_KEY || 'na'
 );
 export async function getStaticProps() {
   const { data } = await supabaseAdmin.from('images').select('*');
